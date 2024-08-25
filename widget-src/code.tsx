d9924d8df3ba7513f6e4969f9cb8bf4a43d4a8f7
Rico <ricoberan@outlook.com>
2024-08-25 12:51:44 +0100

    added input functionality to the widget

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,13 +1,15 @@
 // This is a form builder widget
 
 const { widget } = figma
-const { useSyncedState, usePropertyMenu, AutoLayout, Text, SVG, Frame } = widget
+const { useSyncedState, usePropertyMenu, AutoLayout, Text, SVG, Frame, Input } = widget
 
 // This is the answer widget
 
 function Answer(
   props: Partial<AutoLayoutProps>
 ) {
+  const [answerText, setAnswerText] = useSyncedState("answerText", "");
+
   return (
     <AutoLayout
       name="Answer"
@@ -37,8 +39,11 @@ function Answer(
       width={250}
       {...props}
     >
-      <Text
-        name="Option 1"
+      <Input
+        name="Input"
+        value={answerText}
+        placeholder="Answer..."
+        onTextEditEnd={(event) => setAnswerText(event.characters)}
         fill="#1E1E1E"
         width={204}
         lineHeight="150%"
@@ -46,9 +51,7 @@ function Answer(
         fontSize={14}
         letterSpacing={-0.154}
         fontWeight={500}
-      >
-        Option 1
-      </Text>
+      />
     </AutoLayout>
   );
 }
@@ -58,6 +61,8 @@ function Answer(
 function Header(
   props: Partial<AutoLayoutProps>
 ) {
+  const [headerText, setHeaderText] = useSyncedState("headerText", "");
+
   return (
     <AutoLayout
       name="Header"
@@ -87,8 +92,11 @@ function Header(
       width={250}
       {...props}
     >
-      <Text
-        name="Header Text"
+      <Input
+        name="Input"
+        value={headerText}
+        placeholder="Header..."
+        onTextEditEnd={(event) => setHeaderText(event.characters)}
         fill="#1E1E1E"
         width={204}
         lineHeight="150%"
@@ -96,9 +104,7 @@ function Header(
         fontSize={14}
         letterSpacing={-0.154}
         fontWeight={500}
-      >
-        Header Text
-      </Text>
+      />
     </AutoLayout>
   );
 }
@@ -108,6 +114,8 @@ function Header(
 function Radio(
   props: Partial<AutoLayoutProps>
 ) {
+  const [radioText, setRadioText] = useSyncedState("radioText", "");
+
   console.log("Rendering Radio component with props:", props);
 
   return (
@@ -135,8 +143,11 @@ function Radio(
       width={352}
       {...props}
     >
-      <Text
-        name="Mollit ad pariatur aliquip qui culpa proident anim voluptate."
+      <Input
+        name="Input"
+        value={radioText}
+        placeholder="Question..."
+        onTextEditEnd={(event) => setRadioText(event.characters)}
         fill="#1E1E1E"
         width={306}
         lineHeight="150%"
@@ -144,9 +155,7 @@ function Radio(
         fontSize={18}
         letterSpacing={-0.198}
         fontWeight={500}
-      >
-        Mollit ad pariatur aliquip qui culpa proident anim voluptate.
-      </Text>
+      />
       <AutoLayout
         name="form-builder-badge"
         fill="#FEF9C3"
@@ -185,7 +194,7 @@ function Radio(
           />
         </Frame>
         <Text
-          name="Radio"
+          name="Input"
           fill="#854D0E"
           lineHeight="150%"
           fontFamily="Inter"
@@ -200,10 +209,10 @@ function Radio(
 
 // This is the input widget
 
-function Input(
+function InputWidget(
   props: Partial<AutoLayoutProps>
 ) {
-  console.log("Rendering Input component with props:", props);
+  const [inputText, setInputText] = useSyncedState("inputText", "");
 
   return (
     <AutoLayout
@@ -230,8 +239,11 @@ function Input(
       width={352}
       {...props}
     >
-      <Text
-        name="Mollit ad pariatur aliquip qui culpa proident anim voluptate."
+      <Input
+        name="Input"
+        value={inputText}
+        placeholder="Question..."
+        onTextEditEnd={(event) => setInputText(event.characters)}
         fill="#1E1E1E"
         width={306}
         lineHeight="150%"
@@ -239,9 +251,7 @@ function Input(
         fontSize={18}
         letterSpacing={-0.198}
         fontWeight={500}
-      >
-        Mollit ad pariatur aliquip qui culpa proident anim voluptate.
-      </Text>
+      />
       <AutoLayout
         name="form-builder-badge"
         fill="#FEF9C3"
@@ -287,6 +297,102 @@ function Input(
           fontWeight={600}
         >
           Input
+        </Text>
+      </AutoLayout>
+    </AutoLayout>
+  );
+}
+
+// This is the question widget
+
+function Question(
+  props: Partial<AutoLayoutProps>
+) {
+  const [questionText, setQuestionText] = useSyncedState("questionText", "");
+
+  return (
+    <AutoLayout
+      name="Question"
+      effect={{
+        type: "drop-shadow",
+        color: "#00000014",
+        offset: {
+          x: 0,
+          y: 2,
+        },
+        blur: 14.1,
+        showShadowBehindNode: false,
+      }}
+      fill="#FFF"
+      stroke="#E2E8F0"
+      cornerRadius={16}
+      direction="vertical"
+      spacing={20}
+      padding={{
+        vertical: 27,
+        horizontal: 23,
+      }}
+      width={352}
+      {...props}
+    >
+      <Input
+        name="Input"
+        value={questionText}
+        placeholder="Question..."
+        onTextEditEnd={(event) => setQuestionText(event.characters)}
+        fill="#1E1E1E"
+        width={306}
+        lineHeight="150%"
+        fontFamily="Inter"
+        fontSize={18}
+        letterSpacing={-0.198}
+        fontWeight={500}
+      />
+      <AutoLayout
+        name="form-builder-badge"
+        fill="#FEF9C3"
+        cornerRadius={8}
+        overflow="visible"
+        spacing={4}
+        padding={{
+          vertical: 4,
+          horizontal: 8,
+        }}
+        verticalAlignItems="center"
+      >
+        <Frame
+          name="Icon"
+          strokeWidth={0.833}
+          width={20}
+          height={20}
+        >
+          <SVG
+            name="Vector"
+            x={{
+              type: "horizontal-scale",
+              leftOffsetPercent: 9.375,
+              rightOffsetPercent: 9.375,
+            }}
+            y={{
+              type: "vertical-scale",
+              topOffsetPercent: 9.375,
+              bottomOffsetPercent: 9.375,
+            }}
+            height={16}
+            width={16}
+            src="<svg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
+<path d='M9 0.875008C7.39303 0.875008 5.82214 1.35153 4.486 2.24432C3.14985 3.1371 2.10844 4.40605 1.49348 5.8907C0.87852 7.37535 0.717618 9.00902 1.03112 10.5851C1.34463 12.1612 2.11846 13.6089 3.25476 14.7453C4.39106 15.8816 5.8388 16.6554 7.41489 16.9689C8.99099 17.2824 10.6247 17.1215 12.1093 16.5065C13.594 15.8916 14.8629 14.8502 15.7557 13.514C16.6485 12.1779 17.125 10.607 17.125 9.00001C17.1227 6.84582 16.266 4.78052 14.7427 3.25728C13.2195 1.73404 11.1542 0.877282 9 0.875008ZM9 15.875C7.64026 15.875 6.31105 15.4718 5.18046 14.7164C4.04987 13.9609 3.16868 12.8872 2.64833 11.631C2.12798 10.3747 1.99183 8.99238 2.2571 7.65876C2.52238 6.32514 3.17716 5.10013 4.13864 4.13865C5.10013 3.17716 6.32514 2.52238 7.65876 2.25711C8.99238 1.99184 10.3747 2.12798 11.631 2.64834C12.8872 3.16869 13.9609 4.04987 14.7164 5.18046C15.4718 6.31105 15.875 7.64026 15.875 9.00001C15.8729 10.8227 15.1479 12.5702 13.8591 13.8591C12.5702 15.1479 10.8227 15.8729 9 15.875ZM13.375 9.00001C13.375 9.8653 13.1184 10.7112 12.6377 11.4306C12.157 12.1501 11.4737 12.7108 10.6742 13.042C9.87482 13.3731 8.99515 13.4598 8.14648 13.2909C7.29782 13.1221 6.51827 12.7055 5.90641 12.0936C5.29456 11.4817 4.87788 10.7022 4.70907 9.85353C4.54026 9.00486 4.6269 8.12519 4.95803 7.32577C5.28916 6.52634 5.84992 5.84306 6.56938 5.36233C7.28885 4.8816 8.13471 4.62501 9 4.62501C10.1599 4.62625 11.272 5.08758 12.0922 5.90779C12.9124 6.72799 13.3738 7.84007 13.375 9.00001Z' fill='#854D0E'/>
+</svg>"
+          />
+        </Frame>
+        <Text
+          name="Input"
+          fill="#854D0E"
+          lineHeight="150%"
+          fontFamily="Inter"
+          fontWeight={600}
+        >
+          Question
         </Text>
       </AutoLayout>
     </AutoLayout>
@@ -387,7 +493,7 @@ function Widget(
     } else if (contentType === "header") {
       return <Header {...props} />;
     } else if (contentType === "question") {
-      return questionType === "radio" ? <Radio {...props} /> : <Input {...props} />;
+      return questionType === "radio" ? <Radio {...props} /> : <InputWidget {...props} />;
     } else {
       return <Text>Error: Invalid content type</Text>;
     }
