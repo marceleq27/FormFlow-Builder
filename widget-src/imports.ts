6753ce4e569c68c644df2e086522a85a4076559c
Rico <ricoberan@outlook.com>
2024-08-31 22:26:56 +0100

    consolidate imports

---
+++ widget-src/imports.ts
@@ -0,0 +1,39 @@
+import Header from './components/Header';
+import AnswerTemplate from './components/AnswerTemplate';
+import Input from './components/answertype/Input';
+import Radio from './components/answertype/Radio';
+import Dropdown from './components/answertype/Dropdown';
+import Checkbox from './components/answertype/Checkbox';
+import TextArea from './components/answertype/TextArea';
+import Currency from './components/answertype/Currency';
+import PercentageInput from './components/answertype/PercentageInput';
+import Datepicker from './components/answertype/Datepicker';
+import Fileupload from './components/answertype/Fileupload';
+import Slider from './components/answertype/Slider';
+import Custom from './components/answertype/Custom';
+import Question from './components/Question';
+import { icons } from './icons';
+import { contentTypeOptions } from './constants/contentTypeOptions';
+import { answerTypeOptions } from './constants/answerTypeOptions';
+import { currencyOptions } from './constants/currencyOptions';
+
+export {
+    Header,
+    AnswerTemplate,
+    Input,
+    Radio,
+    Dropdown,
+    Checkbox,
+    TextArea,
+    Currency,
+    PercentageInput,
+    Datepicker,
+    Fileupload,
+    Slider,
+    Custom,
+    Question,
+    icons,
+    contentTypeOptions,
+    answerTypeOptions,
+    currencyOptions
+};
