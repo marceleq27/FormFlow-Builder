a2ad7927b16952df3ecd1e91b4df7086005f4e25
Rico <ricoberan@outlook.com>
2024-09-03 10:10:50 +0100

    updated menu label for answer

--- widget-src/constants/contentTypeOptions.ts
+++ widget-src/constants/contentTypeOptions.ts
@@ -2,7 +2,7 @@ export const contentTypeOptions = [
     { option: "section", label: "Section" },
     { option: "header", label: "Header" },
     { option: "question", label: "Question" },
-    { option: "answer", label: "Answer" },
+    { option: "answer", label: "Response" },
     { option: "note", label: "Note" },
     { option: "devnote", label: "Dev Note" },
     { option: "customcomponent", label: "Custom" },
